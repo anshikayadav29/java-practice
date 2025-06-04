01ebd1e98bac17106495b1c3ffbd436b55e08b38
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-04 22:04:24 +0530

    First commit

---
+++ varletconst.js
@@ -0,0 +1,14 @@
+/*const a =4;
+//value of a is constant ,no allowed any change 
+a =6;
+console.log(a);*/
+
+var a=4;
+console.log(a);
+a ="Mini Yadav";
+console.log(a);
+
+var a=5;
+console.log(a);
+var a=7;
+console.log(a);
