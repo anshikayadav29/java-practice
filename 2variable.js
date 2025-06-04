ec32f12a5cc4402927774811b153e0588f960333
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-04 20:03:06 +0530

    First commit

---
+++ 2variable.js
@@ -0,0 +1,9 @@
+a =5;
+b =6;
+c =9;
+console.log(a,b,c);
+console.log("Mini","is","Lazy","girl");
+age =24;
+//console.log("My age is"+ age +" and i am young");
+console.log("My age is",age," and i am young");
+
