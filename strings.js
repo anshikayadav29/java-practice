4e886992d6d18c415dd33c526470d32a510c7b4c
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-05 22:52:34 +0530

    First commit

---
+++ strings.js
@@ -0,0 +1,15 @@
+/*let a ="Anshika Yadav";
+console.log(a,a.length);
+console.log(a[4]);*/
+
+/*let s ="Anshika Yadav";
+console.log(s,s.length);
+for(let i=0;i<s.length;i++){
+    console.log(s[i]);
+}*/
+
+let s="suffering ";
+console.log(s,s.length);
+for(const char of s){
+    console.log(char);
+}
--- thruthyFalsy.js
+++ thruthyFalsy.js
@@ -3,4 +3,6 @@ if(5>7){
 }
 else console.log("mello");
 /*if print 1,2,3,4,5,6 print any number,('text') except 0
-the outpit is hello(true)*/
+the outpit is hello(true)
+Falsy value -> false condition ,false,0,null,nan,undefined
+truth -> everything except faly value*/
