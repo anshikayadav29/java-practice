f566f65f2ac53c78091fc72bbd1e6131181b4bf5
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-05 13:18:08 +0530

    First commit

---
+++ arrays.js
@@ -0,0 +1,15 @@
+/*arr =[1,6,8,3];
+console.log(arr);*/
+
+
+/*arr =[2,3,5,6,7,56,6,45,44];
+n =arr.length;
+console.log(n);
+console.log(arr[0]);*/
+
+
+arr =[1,4,5,6,3];
+n= arr.length;
+for (let i=0;i<n;i++){
+    console.log(arr[i]);
+}
