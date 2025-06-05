dc5c515e411373a4023db62825be938b08fe6e1a
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-05 14:08:57 +0530

    First commit

---
+++ 3array.js
@@ -0,0 +1,26 @@
+let arr =[2,5,6,5,4];
+arr[0]=9;
+console.log(arr);
+arr =2.4;// this value can not print by const
+console.log(arr);
+
+let c =5;
+console.log(c);
+c ="Mini";
+console.log(c);
+c =true;
+console.log(c);
+c =[2,4,5,3,2];
+console.log(c);
+//but not do this const because if you start 'int' then end with int 
+
+/*const b =[4,5,6,7];
+console.log(b);
+b =10;
+console.log(b) it's not  valid */
+
+//it is  valid
+const b =[4,5,6,7];
+console.log(b);
+b[0]=10;
+console.log(b) 
