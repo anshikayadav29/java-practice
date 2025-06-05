6e36e70c7391441c4dbf96bf709ae607ab0a63fd
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-05 23:34:07 +0530

    First commit

---
+++ stringbuiltin.js
@@ -0,0 +1,25 @@
+/*let s ="Anshika Yadav";
+console.log(s);
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());*/
+
+/*let s= "  Anshika     Yadav";
+console.log(s);
+s.toUpperCase();
+let s2 =s.toUpperCase();
+console.log(s,s2);
+console.log(s.trim());
+
+3rd
+let s ="  Anshika   Yadav";
+console.log(s,s.length);*/
+
+//4rth
+let s ="  Anshika Yadav  ";
+console.log(s,s.length);
+let s2 =s.trim();
+console.log(s2,s2.length);
+console.log(s2.indexOf('h'));
+console.log(s2.lastIndexOf('a'));
+console.log(s2.charAt(3));
+console.log(s2[3]);
