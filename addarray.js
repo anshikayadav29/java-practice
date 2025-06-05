3182389f05df77c2b54755c2981b9dcd7a0b1b2d
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-05 13:35:21 +0530

    First commit

---
+++ addarray.js
@@ -0,0 +1,14 @@
+arr =[1,3,4,5];
+console.log(arr);
+arr.push(9);//add at last
+console.log(arr);
+arr.push(4);
+console.log(arr);
+//remove last element
+arr.pop();
+console.log(arr);
+//unshift -add at first
+arr.unshift(2);
+console.log(arr);
+arr.shift();//remove first element 
+console.log(arr);
