f01c026ed7742b1f7c4e6ce5becb89afdd3fc08f
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-06 07:53:45 +0530

    First commit

---
+++ slicestring.js
@@ -0,0 +1,8 @@
+let s ="Anshikayadav";
+/*console.log(s.slice(0));
+console.log(s.slice(3));*/
+console.log(s.slice(2,6));//substring from 2 t0 5console.log(s.slice(0));
+/*s.slice(2,5);
+console.log(s);*/
+s2 =s.slice(2,5);
+console.log(s,s2);
