3523cbcc23b3e412aa1d402a0914db6309bf641d
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-06 08:05:18 +0530

    First commit

---
+++ 3stringsplit.js
@@ -0,0 +1,16 @@
+/*let s ="Raghav is a teacher at physics wallah";
+console.log(s);
+let arr =s.split(' ');
+//console.log(arr);
+for(const ele of arr){
+    console.log(ele);
+
+}*/
+
+
+let s ="a,gh,b,rtf,w,v";
+console.log(s);
+let arr =s.split(',');
+for (const ele of arr){
+    console.log(ele);
+}
