f10d21fd5c4bfa2a5d10cface592af662f9d895f
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-06 20:22:05 +0530

    First commit

---
+++ mathmaticalfunction.js
@@ -0,0 +1,14 @@
+console.log(Math.abs(-7));
+console.log(Math.max(5,1));
+console.log(Math.min(-8,6));
+console.log(Math.max(4,6,90,599,2003));
+console.log(Math.pow(2,7,7));
+console.log(Math.sqrt(10))
+console.log(Math.cbrt(582));
+
+console.log(Math.log10(10000));
+//convert decimal to integer
+console.log(Math.floor(5.8));
+console.log(Math.ceil(5.8));
+//random value
+console.log(Math.random());
