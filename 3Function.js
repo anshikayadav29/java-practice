5dbdb9c2056df9d1f4b3010b95574b5ca6d0023a
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-06 17:26:33 +0530

    First commit

---
+++ 3Function.js
@@ -0,0 +1,15 @@
+/*function OnetoN(n){
+for (let i=1;i<=n;i++){
+    console.log(i);
+    }
+    console.log();
+  }
+
+  OnetoN(19);
+  OnetoN(14);
+  OnetoN(7);*/
+  function eqn(a,b){
+    return Math.abs(a*a*a)+Math.abs(b*b*b);
+
+  }
+  console.log(eqn(2,4));
