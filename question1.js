a16aa24849f050c66a4b0c4fa894ca7c5c245313
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-06 20:33:30 +0530

    First commit

---
+++ question1.js
@@ -0,0 +1,12 @@
+// to Generate a random number between 0 and 9 included
+/*let num =Math.random()*10;
+console.log(num);*/
+
+//without decimal
+let num =Math.floor(Math.random()*10);
+console.log(num);
+
+if(Math.random()<8.5){
+    console.log("Hello");
+}
+else console.log("bye");
