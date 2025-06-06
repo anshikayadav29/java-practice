2592602341ce53216c7ccf5485a790e5152dc5a2
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-06 09:53:47 +0530

    First commit

---
+++ object.js
@@ -0,0 +1,12 @@
+//object is kind of like/dictionary where we have key-value pairs
+var details =["Anshika",18,96.6,false];
+
+let x ={
+    name:"Anshika",
+    "nick name":"Anshi",
+    age: 24,
+    percentage: 96.6,
+    isMarried:false
+};
+console.log(x);
+//for only age ->console.log(x.age);
