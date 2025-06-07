24e83cbb5efe5c55c03c239edbaccb864029d00a
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-07 12:07:32 +0530

    First commit

---
+++ sort.js
@@ -0,0 +1,15 @@
+/*let arr =[1,9,2,7];
+console.log(arr);
+
+arr =arr.sort();
+console.log(arr);*/
+
+
+//2nd program
+let arr =[1,-9,-2,7];
+console.log(arr);
+
+arr =arr.sort((a,b) =>Math.abs(a)-Math.abs(b));
+console.log(arr);
+
+
