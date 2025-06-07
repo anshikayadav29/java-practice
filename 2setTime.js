7f6dfaf98f9699d5ab7df057503086bb64fea960
anshikayadav11 <anshikayadav2917@gmail.com>
2025-06-07 07:49:47 +0530

    First commit

---
+++ 2setTime.js
@@ -0,0 +1,24 @@
+/*setTimeout(function(){
+    console.log("hello");
+},2*1000);
+setTimeout(function(){
+    console.log("mello");
+},1*1000);
+setTimeout(function(){
+    console.log("mini");
+},5*1000)*/
+
+
+//print 1 to 10
+
+/*for (let i=1;i<=10;i++){
+    setTimeout(function(){
+        console.log(i);
+    } ,i*1000)
+}*/
+
+for (let i=1;i<=10;i++){
+    setTimeout(function(){
+        console.log(11-i);
+    } ,i*1000)
+}
